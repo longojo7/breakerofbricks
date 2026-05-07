7005143e1c606dd0609114ba1ff0c473e208fed2
Josh Longo <longo.jo@northeastern.edu>
2026-05-07 00:55:56 -0400

    Implemented wall bouncing, paddle collision, and angle of ball control on paddle

--- break.c
+++ break.c
@@ -12,6 +12,7 @@
 #define BALL   "o"
 #define BRICK  "####"
 #define BRICK_WIDTH 5
+#define PADDLE_WIDTH 7
 
 WINDOW* create_game_win(int start_y, int start_x) {
 	WINDOW* game_win = newwin(G_HEIGHT, G_WIDTH, start_y, start_x);
@@ -59,12 +60,13 @@ int main() {
 	init_pair(3, COLOR_RED, COLOR_RED);
 
 	// set the start x and start y to center the two windows
-	int startx = (COLS - (G_WIDTH + S_WIDTH)) / 2;
+	int space = 1;
+	int startx = (COLS - (G_WIDTH + S_WIDTH + space)) / 2;
 	int starty = (LINES - G_HEIGHT) / 2;
 	
 	// create the game and score window
 	WINDOW* game_win = create_game_win(starty, startx);
-	WINDOW* score_win = create_score_win(starty, G_WIDTH + startx);
+	WINDOW* score_win = create_score_win(starty, startx + G_WIDTH + space);
 
 	// initialize coordinates and deltas
 	int paddle_x = (G_WIDTH - 2) / 2 - 2;
@@ -72,6 +74,7 @@ int main() {
 	int ball_y = BALL_Y;
 	int ball_dx = 1;
 	int ball_dy = -1;
+	int game_start = 0;
 
 	// getch() won't block the game loop so game will
 	// keep running when player does not press anything
@@ -86,20 +89,37 @@ int main() {
 		// be able to move paddle to the left and right
 		if (ch == KEY_LEFT && paddle_x > 1) {
 			paddle_x--;
-			ball_x--;
+			if (!game_start) ball_x--;
 		}
 		if (ch == KEY_RIGHT && paddle_x < 33) {
 			paddle_x++;
-			ball_x++;
+			if (!game_start) ball_x++;
 		}
 
-		// update the ball position
-		ball_x += ball_dx;
-		ball_y += ball_dy;
+		// add a play button
+		if (ch == 'p') game_start = 1;
+		if (game_start) {
+			// update the ball position
+			ball_x += ball_dx;
+			ball_y += ball_dy;
 
-		// check if the ball is bouncing off the wall
-		if (ball_x <= 1 || ball_x >= G_WIDTH - 2) ball_dx = -ball_dx;
-		if (ball_y <= 1 || ball_y >= G_HEIGHT - 2) ball_dy = -ball_dy;
+			// check if the ball is bouncing off the wall
+			if (ball_x <= 1 || ball_x >= G_WIDTH - 2) ball_dx = -ball_dx;
+			// set to 0 because it looks better with ball hitting the top border
+			if (ball_y <= 0) ball_dy = -ball_dy;
+
+			// check if the ball has bounced off the paddle,give one col past paddle width for wider collision
+			if (ball_x >= paddle_x - 1 && ball_x <= paddle_x + PADDLE_WIDTH && ball_y == PADDLE_Y - 1) {
+				ball_dy = -ball_dy;
+
+				// ball moves left on left side of paddle, right on right side of paddle
+				if (ball_x < paddle_x + 2) {
+					ball_dx = -1; 
+				} else if (ball_x > paddle_x + 4) {
+					ball_dx = 1;
+				}
+			}
+		}
 
 		// erase both windows
 		werase(game_win);
@@ -125,7 +145,7 @@ int main() {
 		int brick_start = 1;
 		for (int i = 0; i < 5; i += 2) {
 			for (int j = 0; j < 8; j++) {
-				mvwprintw(game_win, 1 + i, brick_start + (BRICK_WIDTH * j), BRICK);
+				mvwprintw(game_win, 3 + i, brick_start + (BRICK_WIDTH * j), BRICK);
 			}
 		}
 		wattroff(game_win, COLOR_PAIR(3));
